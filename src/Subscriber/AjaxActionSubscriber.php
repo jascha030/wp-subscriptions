f3df5deb767fbb61dea737344a6bfbd51037b6b7
Jascha van Aalst <mr.jascha@gmail.com>
2020-03-12 13:53:48 +0100

    Ignore magic methods

--- src/Subscriber/AjaxActionSubscriber.php
+++ src/Subscriber/AjaxActionSubscriber.php
@@ -4,26 +4,28 @@ namespace Jascha030\WPSI\Subscriber;
 
 class AjaxActionSubscriber implements ActionSubscriber
 {
+    USE Subscriber;
+    
     const WP_AJAX = "wp_ajax_";
 
     const NOPRIV = "wp_ajax_nopriv_";
 
-    USE Subscriber;
-
     protected static $actions = [];
 
-    private $ignoredMethods = [];
+    protected $ignoredMethods = [];
 
-    public function __construct($ignore = [])
+    private $enforceAdminPrivilege;
+
+    public function __construct($ignore = [], $enforceAdminPrivilege = false)
     {
-        if (! empty($ignore)) {
-            $this->ignoredMethods = array_merge($this->ignoredMethods, $ignore);
-        }
+        $this->ignoredMethods = array_merge($this->ignoredMethods, $ignore);
+
+        $this->enforceAdminPrivilege = $enforceAdminPrivilege;
 
         $methods = get_class_methods($this);
 
         foreach ($methods as $method) {
-            if (! in_array($method, $this->ignoredMethods) && $method !== "__construct" && ! method_exists($method,
+            if (! in_array($method, $this->ignoredMethods) && ! strpos($method, "__") && ! method_exists($method,
                     Subscriber::class)) {
 
                 self::$actions[self::WP_AJAX] = [$this, $method];
