0f17cdd30a747057e7e24950aee318850ae80f61
Jascha van Aalst <mr.jascha@gmail.com>
2020-03-12 11:10:24 +0100

    Manager methods

--- src/Subscription/SubscriptionManager.php
+++ src/Subscription/SubscriptionManager.php
@@ -2,7 +2,65 @@
 
 namespace Jascha030\WPSI\Subscription;
 
+use Jascha030\WPSI\Subscriber\ActionSubscriber;
+use Jascha030\WPSI\Subscriber\FilterSubscriber;
+use Jascha030\WPSI\Subscriber\PluginSubscriber;
+
 class SubscriptionManager
 {
+    public static function register(PluginSubscriber $object)
+    {
+        $filters       = ($object instanceof ActionSubscriber) ? self::getActionSubscriptions($object) : [];
+        $actions       = ($object instanceof FilterSubscriber) ? self::getFilterSubscriptions($object) : [];
+        $subscriptions = array_merge($actions, $filters);
 
+        self::subscribeToAll($subscriptions);
+    }
+
+    private static function getActionSubscriptions(ActionSubscriber $subscriber)
+    {
+        $actions = [];
+
+        if ($subscriber->getActions()) {
+            foreach ($subscriber->getActions() as $hook => $parameters) {
+                if (is_array($parameters) && is_array($parameters[0])) {
+                    foreach ($parameters[0] as $actionParams) {
+                        $actions[] = new ActionSubscription($hook, $subscriber, $actionParams);
+                    }
+                } else {
+                    $actions[] = new ActionSubscription($hook, $subscriber, $parameters);
+                }
+            }
+        }
+
+        return $actions;
+    }
+
+    private static function getFilterSubscriptions(FilterSubscriber $subscriber)
+    {
+        $filters = [];
+
+        if ($subscriber->getFilters()) {
+            foreach ($subscriber->getFilters() as $hook => $parameters) {
+                if (is_array($parameters) && is_array($parameters[0])) {
+                    foreach ($parameters[0] as $filterParams) {
+                        $filters[] = new FilterSubscription($hook, $subscriber, $filterParams);
+                    }
+                } else {
+                    $filters[] = new FilterSubscription($hook, $subscriber, $parameters);
+                }
+            }
+        }
+
+        return $filters;
+    }
+
+    private static function subscribeToAll($subscriptions)
+    {
+        foreach ($subscriptions as $subscription) {
+            if ($subscription instanceof Subscription) {
+                $subscription->subscribe();
+            }
+        }
+    }
 }
