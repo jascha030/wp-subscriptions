e62f0d289659b5d4b31a8b4303d688b94adaaccf
Jascha van Aalst <mr.jascha@gmail.com>
2020-03-25 20:53:59 +0100

    Added Exceptions

---
+++ src/Exception/SubscriptionException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Jascha030\WPSI\Exception;
+
+use Exception;
+
+/**
+ * Class SubscriptionError
+ *
+ * @package Jascha030\WPSI\Exception
+ */
+class SubscriptionError extends Exception {}
