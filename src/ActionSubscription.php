561cb7e35711365434c4806f992d4af9aec28413
Jascha van Aalst <mr.jascha@gmail.com>
2020-05-26 01:46:59 +0200

    Optimise Action/Filter/Hook Subscription classes

--- src/ActionSubscription.php
+++ src/ActionSubscription.php
@@ -13,56 +13,22 @@ use Jascha030\WP\Subscriptions\Exception\SubscriptionException;
  */
 class ActionSubscription extends HookSubscription implements Unsubscribable
 {
-    /**
-     * @var int
-     */
-    private $priority;
+    const METHOD = 'add_filter';
+
+    const UNSUB_METHOD = 'remove_filter';
 
     /**
-     * @var int
-     */
-    private $acceptedArguments;
-
-    /**
-     * ActionSubscription constructor.
+     * FilterSubscription constructor.
      *
      * @param $tag
      * @param $callable
      * @param int $priority
      * @param int $acceptedArguments
      *
-     * @throws InvalidArgumentException
+     * @throws \Jascha030\WP\Subscriptions\Exception\InvalidArgumentException
      */
     public function __construct($tag, $callable, $priority = 10, $acceptedArguments = 1)
     {
-        parent::__construct($tag, $callable);
-
-        $this->priority = $priority;
-
-        $this->acceptedArguments = $acceptedArguments;
-    }
-
-    /**
-     * @throws NotCallableException
-     * @throws SubscriptionException
-     */
-    public function subscribe()
-    {
-        parent::subscribe();
-
-        add_action($this->tag, $this->callable, $this->priority, $this->acceptedArguments);
-    }
-
-    /**
-     * @throws SubscriptionException
-     */
-    public function unsubscribe()
-    {
-        if ($this->isActive()) {
-            throw new SubscriptionException("Can't unsubscribe before subscribing");
-        } else {
-            remove_action($this->tag, $this->callable, $this->priority, $this->acceptedArguments);
-            $this->active = false;
-        }
+        parent::__construct($tag, $callable, $priority, $acceptedArguments);
     }
 }
